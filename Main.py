eacbf9edeb6414660ace95c280b14fa0ba46fa3c
PhoenixAki <seanstan95@gmail.com>
2019-09-27 06:21:29 -0400

    Project 2 Start - added Setup function to allow for restarting the game - prevent player from shooting more than once per second

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ IMAGE_WIDTH = 820
 SCREEN_WIDTH = 820
 SCREEN_HEIGHT = 410
 SCREEN_TITLE = "Project 1"
-SHIP_PATH = pathlib.Path.cwd() / "Resources" / "Ship.png"
+PLAYER_PATH = pathlib.Path.cwd() / "Resources" / "Player.png"
 BULLET_PATH = pathlib.Path.cwd() / "Resources" / "Bullet.png"
 BACKGROUND_PATH = pathlib.Path.cwd() / "Resources" / "Background.png"
 LASER_PATH = pathlib.Path.cwd() / "Resources" / "Laser.wav"
@@ -23,30 +23,42 @@ class MyGame(arcade.Window):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.BLACK)
 
-        # setup done here instead of a setup function to avoid warnings about initialization outside of __init__
-        # sprite lists for referencing/appending later
+        # sprite lists to manage shots and background
         self.bullet_list = arcade.SpriteList()
         self.background_list = arcade.SpriteList()
-        # initial ship position and speed
-        self.ship_x = 100
-        self.ship_y = SCREEN_HEIGHT / 2
+        # player position and speed, speed never changes
+        self.player_x = 0
+        self.player_y = 0
         self.SHIP_SPEED = 200
-        # boolean flags for movement logic in on_update
+        # flags for movement logic in on_update
         self.right = False
         self.left = False
         self.up = False
         self.down = False
+        # timers
+        self.shot_timer = 0
         # pathlib to ensure working directory regardless of platform
         self.laser_sound = arcade.Sound(LASER_PATH)
-        self.ship = arcade.Sprite(SHIP_PATH, center_x=300, center_y=50)
-        self.background1 = arcade.Sprite(BACKGROUND_PATH, center_x=IMAGE_WIDTH/2, center_y=SCREEN_HEIGHT/2)
-        self.background2 = arcade.Sprite(BACKGROUND_PATH, center_x=SCREEN_WIDTH+IMAGE_WIDTH/2, center_y=SCREEN_HEIGHT/2)
+        self.ship = arcade.Sprite(PLAYER_PATH)
+        self.background1 = arcade.Sprite(BACKGROUND_PATH, center_x=IMAGE_WIDTH / 2, center_y=SCREEN_HEIGHT / 2)
+        self.background2 = arcade.Sprite(BACKGROUND_PATH, center_x=SCREEN_WIDTH + IMAGE_WIDTH / 2, center_y=SCREEN_HEIGHT / 2)
 
         # set up infinite scrolling background logic
         self.background1.change_x = -3
-        self.background2.change_x = -3
         self.background_list.append(self.background1)
+        self.background2.change_x = -3
         self.background_list.append(self.background2)
+
+    def setup(self):
+        # reset variables so player can restart
+        self.bullet_list = arcade.SpriteList()
+        self.player_x = 100
+        self.player_y = SCREEN_HEIGHT / 2
+        self.right = False
+        self.left = False
+        self.up = False
+        self.down = False
+        self.shot_timer = 1
 
     def on_draw(self):
         arcade.start_render()
@@ -57,27 +69,30 @@ class MyGame(arcade.Window):
     def on_update(self, delta_time):
         # check the flags for movement and update accordingly
         if self.right:
-            self.ship_x += self.SHIP_SPEED * delta_time
+            self.player_x += self.SHIP_SPEED * delta_time
         if self.left:
-            self.ship_x -= self.SHIP_SPEED * delta_time
+            self.player_x -= self.SHIP_SPEED * delta_time
         if self.up:
-            self.ship_y += self.SHIP_SPEED * delta_time
+            self.player_y += self.SHIP_SPEED * delta_time
         if self.down:
-            self.ship_y -= self.SHIP_SPEED * delta_time
+            self.player_y -= self.SHIP_SPEED * delta_time
 
         # prevent movement outside of the window boundaries
-        if self.ship_x > SCREEN_WIDTH - 38:
-            self.ship_x -= self.SHIP_SPEED * delta_time
-        if self.ship_x < 38:
-            self.ship_x += self.SHIP_SPEED * delta_time
-        if self.ship_y > SCREEN_HEIGHT - 17:
-            self.ship_y -= self.SHIP_SPEED * delta_time
-        if self.ship_y < 17:
-            self.ship_y += self.SHIP_SPEED * delta_time
+        if self.player_x > SCREEN_WIDTH - 38:
+            self.player_x -= self.SHIP_SPEED * delta_time
+        if self.player_x < 38:
+            self.player_x += self.SHIP_SPEED * delta_time
+        if self.player_y > SCREEN_HEIGHT - 17:
+            self.player_y -= self.SHIP_SPEED * delta_time
+        if self.player_y < 17:
+            self.player_y += self.SHIP_SPEED * delta_time
 
         # update position of ship and any active bullets
-        self.ship.set_position(self.ship_x, self.ship_y)
+        self.ship.set_position(self.player_x, self.player_y)
         self.bullet_list.update()
+
+        # progress timers
+        self.shot_timer += delta_time
 
         # side-scrolling background logic
         if self.background1.left <= -820:
@@ -98,7 +113,8 @@ class MyGame(arcade.Window):
         if symbol == arcade.key.DOWN or symbol == arcade.key.S:
             self.down = True
 
-        if symbol == arcade.key.SPACE:
+        if symbol == arcade.key.SPACE and self.shot_timer >= 1:
+            self.shot_timer = 0
             self.laser_sound.play()
             bullet = Bullet(BULLET_PATH)
             bullet.center_x = self.ship.center_x + 76
@@ -129,6 +145,7 @@ class Bullet(arcade.Sprite):
 
 def main():
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
     arcade.run()
 
 

3e16f3b3c4e3e22b923ade27efc938737339681f
Phoenix Sean <seanstan95@gmail.com>
2019-10-02 06:40:21 -0400

    Player Shot Collision - Player shots now collide with enemies and both objects are destroyed on impact. - Cropped Sprite .png's to have their hitboxes be more accurate. - Other general reorganizations/renames.

--- Main.py
+++ Main.py
@@ -12,13 +12,13 @@ IMAGE_WIDTH = 820
 SCREEN_WIDTH = 820
 SCREEN_HEIGHT = 410
 SCREEN_TITLE = "Project 1"
-PLAYER_PATH = pathlib.Path.cwd() / "Resources" / "Player.png"
-ENEMY_PATH = pathlib.Path.cwd() / "Resources" / "Enemy1.png"
-PLAYER_SHOT_PATH = pathlib.Path.cwd() / "Resources" / "PlayerShot.png"
-ENEMY_SHOT_PATH = pathlib.Path.cwd() / "Resources" / "EnemyShot1.png"
-BACKGROUND_PATH = pathlib.Path.cwd() / "Resources" / "Background.png"
-PLAYER_SHOT_SOUND_PATH = pathlib.Path.cwd() / "Resources" / "PlayerShot.wav"
-# ENEMY_SHOT_SOUND_PATH = pathlib.Path.cwd() / "Resources" / "EnemyShot1.wav"
+PLAYER_PATH = str(pathlib.Path.cwd() / "Resources" / "Player.png")
+ENEMY_PATH = str(pathlib.Path.cwd() / "Resources" / "Enemy1.png")
+PLAYER_SHOT_PATH = str(pathlib.Path.cwd() / "Resources" / "PlayerShot.png")
+ENEMY_SHOT_PATH = str(pathlib.Path.cwd() / "Resources" / "EnemyShot1.png")
+BACKGROUND_PATH = str(pathlib.Path.cwd() / "Resources" / "Background.png")
+PLAYER_SHOT_SOUND_PATH = str(pathlib.Path.cwd() / "Resources" / "PlayerShot.wav")
+ENEMY_SHOT_SOUND_PATH = str(pathlib.Path.cwd() / "Resources" / "PlayerShot.wav") # placeholder until proper sound picked
 
 
 class MyGame(arcade.Window):
@@ -27,59 +27,99 @@ class MyGame(arcade.Window):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.BLACK)
 
-        # sprite lists to manage shots and background
+        # sprite lists for enemies, shots, and background
         self.enemy_list = None
-        self.shot_list = None
+        self.player_shot_list = None
+        self.enemy_shot_list = None
+        self.collisions = None
         self.background_list = arcade.SpriteList()
-        # player speed, never changes
+        # timers and player speed (never changes)
+        self.enemy_timer = 0
+        self.player_shot_timer = 0
+        self.enemy_shot_timer = 0
         self.PLAYER_SPEED = 200
-        # flags for movement logic in on_update
+        # flags for player movement logic
         self.right = None
         self.left = None
         self.up = None
         self.down = None
-        # timers
-        self.shot_timer = 0
-        self.enemy_timer = 0
-        self.enemy_shot_timer = 0
         # pathlib to ensure working directory regardless of platform
-        self.laser_sound = arcade.Sound(PLAYER_SHOT_SOUND_PATH)
+        self.player_shot_sound = arcade.Sound(PLAYER_SHOT_SOUND_PATH)
+        self.enemy_shot_sound = arcade.Sound(ENEMY_SHOT_SOUND_PATH)
         self.player = arcade.Sprite(PLAYER_PATH)
-        self.enemy_rand = 0
+        # placeholders
         self.enemy_shooting = None
         self.new_enemy = None
         self.shot = None
-        self.background1 = arcade.Sprite(BACKGROUND_PATH, center_x=IMAGE_WIDTH / 2, center_y=SCREEN_HEIGHT / 2)
-        self.background2 = arcade.Sprite(BACKGROUND_PATH, center_x=SCREEN_WIDTH+IMAGE_WIDTH/2, center_y=SCREEN_HEIGHT/2)
+        self.enemy_rand = 0
 
         # set up infinite scrolling background logic
+        self.background1 = arcade.Sprite(BACKGROUND_PATH, center_x=IMAGE_WIDTH / 2, center_y=SCREEN_HEIGHT / 2)
+        self.background2 = arcade.Sprite(BACKGROUND_PATH, center_x=SCREEN_WIDTH+IMAGE_WIDTH/2, center_y=SCREEN_HEIGHT/2)
         self.background1.change_x = -3
-        self.background_list.append(self.background1)
         self.background2.change_x = -3
+        self.background_list.append(self.background1)
         self.background_list.append(self.background2)
 
     def setup(self):
         # reset variables so player can restart
-        self.shot_list = arcade.SpriteList()
+        self.player_shot_list = arcade.SpriteList()
+        self.enemy_shot_list = arcade.SpriteList()
+        self.collisions = arcade.SpriteList()
         self.enemy_list = arcade.SpriteList()
         self.player.center_x = 100
         self.player.center_y = SCREEN_HEIGHT / 2
-        self.shot_timer = 1
+        self.player_shot_timer = 1
         self.enemy_timer = 5
         self.right = False
         self.left = False
         self.up = False
         self.down = False
-        self.shot_timer = 1
+        self.player_shot_timer = 1
 
     def on_draw(self):
         arcade.start_render()
         self.background_list.draw()
         self.player.draw()
         self.enemy_list.draw()
-        self.shot_list.draw()
+        self.player_shot_list.draw()
+        self.enemy_shot_list.draw()
 
     def on_update(self, delta_time):
+        # some code in other functions to save clutter here
+        self.move_player(delta_time)
+        self.move_background()
+        self.check_collisions()
+
+        # update timers and sprite lists
+        self.enemy_timer += delta_time
+        self.enemy_list.update()
+        self.player_shot_timer += delta_time
+        self.player_shot_list.update()
+        self.enemy_shot_list.update()
+        if len(self.enemy_list) > 0:
+            self.enemy_shot_timer += delta_time
+
+        # spawning enemies every 5 seconds
+        if self.enemy_timer >= 5:
+            self.enemy_timer = 0
+            self.new_enemy = Enemy(ENEMY_PATH, "Enemy", 1.5, .75)
+            self.new_enemy.center_x = 880
+            self.new_enemy.center_y = random.randint(27, SCREEN_HEIGHT-27)
+            self.enemy_list.append(self.new_enemy)
+
+        # enemies fire every 1.5 seconds
+        if self.enemy_shot_timer >= 1.5:
+            self.enemy_shot_timer = 0
+            self.enemy_shot_sound.play()
+            self.enemy_rand = random.randint(0, len(self.enemy_list)-1)
+            self.enemy_shooting = self.enemy_list.sprite_list[self.enemy_rand]
+            self.shot = Shot(ENEMY_SHOT_PATH, -8.5, 2)
+            self.shot.center_x = self.enemy_shooting.center_x - 35
+            self.shot.center_y = self.enemy_shooting.center_y
+            self.enemy_shot_list.append(self.shot)
+
+    def move_player(self, delta_time):
         # check the flags for movement and update accordingly
         if self.right:
             self.player.center_x += self.PLAYER_SPEED * delta_time
@@ -100,40 +140,23 @@ class MyGame(arcade.Window):
         if self.player.center_y < 17:
             self.player.center_y += self.PLAYER_SPEED * delta_time
 
-        # update timers and sprite lists
-        self.enemy_timer += delta_time
-        self.enemy_list.update()
-        self.shot_timer += delta_time
-        self.shot_list.update()
-
-        if len(self.enemy_list) > 0:
-            self.enemy_shot_timer += delta_time
-
-        # spawning enemies
-        if self.enemy_timer >= 5:
-            self.enemy_timer = 0
-            self.new_enemy = Enemy(ENEMY_PATH, scale=.75)
-            self.new_enemy.center_x = 880
-            self.new_enemy.center_y = random.randint(27, SCREEN_HEIGHT-27)
-            self.enemy_list.append(self.new_enemy)
-
-        # enemy firing
-        if self.enemy_shot_timer >= 1.5:
-            self.enemy_shot_timer = 0
-            self.enemy_rand = random.randint(0, len(self.enemy_list)-1)
-            self.enemy_shooting = self.enemy_list.sprite_list[self.enemy_rand]
-            self.shot = Shot(ENEMY_SHOT_PATH, -8.5)
-            self.shot.center_x = self.enemy_shooting.center_x - 35
-            self.shot.center_y = self.enemy_shooting.center_y
-            self.shot_list.append(self.shot)
-
+    def move_background(self):
         # side-scrolling background logic
         if self.background1.left <= -820:
-            self.background1.center_x = SCREEN_WIDTH + IMAGE_WIDTH//2
+            self.background1.center_x = SCREEN_WIDTH + IMAGE_WIDTH // 2
         if self.background2.left <= -820:
-            self.background2.center_x = SCREEN_WIDTH + IMAGE_WIDTH//2
+            self.background2.center_x = SCREEN_WIDTH + IMAGE_WIDTH // 2
 
         self.background_list.update()
+
+    def check_collisions(self):
+        for shot in self.player_shot_list:
+            self.collisions = shot.collides_with_list(self.enemy_list)
+            for enemy in self.collisions:
+                if enemy.name == "Enemy":
+                    # self.enemy_explosion.play()
+                    enemy.kill()
+                    shot.kill()
 
     def on_key_press(self, symbol, modifiers):
         # check for keyboard input on arrows or W/A/S/D
@@ -146,13 +169,13 @@ class MyGame(arcade.Window):
         if symbol == arcade.key.DOWN or symbol == arcade.key.S:
             self.down = True
 
-        if symbol == arcade.key.SPACE and self.shot_timer >= 1:
-            self.shot_timer = 0
-            self.laser_sound.play()
-            self.shot = Shot(PLAYER_SHOT_PATH, 10)
+        if symbol == arcade.key.SPACE and self.player_shot_timer >= 1:
+            self.player_shot_timer = 0
+            self.player_shot_sound.play()
+            self.shot = Shot(PLAYER_SHOT_PATH, 10, 1)
             self.shot.center_x = self.player.center_x + 76
             self.shot.center_y = self.player.center_y
-            self.shot_list.append(self.shot)
+            self.player_shot_list.append(self.shot)
 
         if symbol == arcade.key.ESCAPE:
             self.close()
@@ -170,27 +193,30 @@ class MyGame(arcade.Window):
 
 
 class Shot(arcade.Sprite):
-    def __init__(self, filename, x):
-        super().__init__(filename)
-        self.speed = x
+    def __init__(self, filename, speed, change_scale):
+        super().__init__(filename, scale=change_scale)
+        self.speed = speed
 
     def update(self):
         self.center_x += self.speed
-        if self.center_x > SCREEN_WIDTH or self.center_x < 0:
+        if self.right > SCREEN_WIDTH or self.center_x < 0:
             self.kill()
 
 
 class Enemy(arcade.Sprite):
-    SPEED = 1.5
+    def __init__(self, filename, name, speed, sprite_scale):
+        super().__init__(filename, scale=sprite_scale)
+        self.name = name
+        self.speed = speed
 
     def update(self):
         # enter screen then move up/down, reversing movement when hitting window boundaries
         if self.center_x > 770:
             self.center_x -= 1
         else:
-            self.center_y += self.SPEED
+            self.center_y += self.speed
             if self.center_y > SCREEN_HEIGHT - 27 or self.center_y < 27:
-                self.SPEED *= -1
+                self.speed *= -1
 
 
 def main():

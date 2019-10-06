cb1a40e310bfe7ee25c086827ba17317f0772c64
Phoenix Sean <seanstan95@gmail.com>
2019-10-06 05:29:50 -0400

    Final Additions - Added lose-game sound (couldn't find any good free ones so used Mario). - Changed from checking center_x/center_y + an offset, to checking top, bottom, etc. - Changed Enemy1 to be the boss Enemy2 and replaced with a smaller enemy. - Enemy2 moves faster and shots travel faster, takes 2 hits to kill, and changes color on first hit. - Finishing touches/organization will come later in the day.

--- Main.py
+++ Main.py
@@ -12,17 +12,20 @@ IMAGE_WIDTH = 820
 SCREEN_WIDTH = 820
 SCREEN_HEIGHT = 410
 SCREEN_TITLE = "Project 1"
-PLAYER_PATH = str(pathlib.Path.cwd() / "Resources" / "Player.png")
-ENEMY_PATH = str(pathlib.Path.cwd() / "Resources" / "Enemy1.png")
-PLAYER_SHOT_PATH = str(pathlib.Path.cwd() / "Resources" / "PlayerShot.png")
+ENEMY1_PATH = str(pathlib.Path.cwd() / "Resources" / "Enemy1.png")
+ENEMY2_PATH = str(pathlib.Path.cwd() / "Resources" / "Enemy2.png")
+ENEMY2_DAMAGED_PATH = str(pathlib.Path.cwd() / "Resources" / "Enemy2Damaged.png")
 ENEMY_SHOT_PATH = str(pathlib.Path.cwd() / "Resources" / "EnemyShot1.png")
+ENEMY_SHOT2_PATH = str(pathlib.Path.cwd() / "Resources" / "EnemyShot2.png")
+PLAYER_PATH = str(pathlib.Path.cwd() / "Resources" / "Player.png")
+PLAYER_SHOT_PATH = str(pathlib.Path.cwd() / "Resources" / "PlayerShot.png")
 BACKGROUND_PATH = str(pathlib.Path.cwd() / "Resources" / "Background.png")
 PLAYER_SHOT_SOUND_PATH = str(pathlib.Path.cwd() / "Resources" / "PlayerShot.wav")
 PLAYER_HIT_SOUND_PATH = str(pathlib.Path.cwd() / "Resources" / "PlayerHit.wav")
 ENEMY_SHOT_SOUND_PATH = str(pathlib.Path.cwd() / "Resources" / "EnemyShot.wav")
 ENEMY_HIT_SOUND_PATH = str(pathlib.Path.cwd() / "Resources" / "EnemyHit.wav")
 WIN_SOUND_PATH = str(pathlib.Path.cwd() / "Resources" / "Win.wav")
-# LOSE_SOUND_PATH = str(pathlib.Path.cwd() / "Resources" / "Lose.wav")
+LOSE_SOUND_PATH = str(pathlib.Path.cwd() / "Resources" / "Lose.wav")
 
 
 class MyGame(arcade.Window):
@@ -46,17 +49,17 @@ class MyGame(arcade.Window):
         self.state = None
         self.score = 0
         # flags for player movement logic
-        self.right = None
-        self.left = None
-        self.up = None
-        self.down = None
+        self.move_right = None
+        self.move_left = None
+        self.move_up = None
+        self.move_down = None
         # initializing sounds and sprites
         self.player_shot_sound = arcade.Sound(PLAYER_SHOT_SOUND_PATH)
         self.player_hit_sound = arcade.Sound(PLAYER_HIT_SOUND_PATH)
         self.enemy_shot_sound = arcade.Sound(ENEMY_SHOT_SOUND_PATH)
         self.enemy_hit_sound = arcade.Sound(ENEMY_HIT_SOUND_PATH)
         self.win_sound = arcade.Sound(WIN_SOUND_PATH)
-        # self.lose_sound = arcade.Sound(LOSE_SOUND_PATH)
+        self.lose_sound = arcade.Sound(LOSE_SOUND_PATH)
         self.player = arcade.Sprite(PLAYER_PATH)
         # other temp variables
         self.enemy_shooting = None
@@ -89,10 +92,10 @@ class MyGame(arcade.Window):
         self.enemy_timer = 3
         self.player_health = 2
         self.enemy_spawn_time = 4
-        self.right = False
-        self.left = False
-        self.up = False
-        self.down = False
+        self.move_right = False
+        self.move_left = False
+        self.move_up = False
+        self.move_down = False
         self.state = "RUNNING"  # reset state to running so movement is allowed
         self.player_shot_timer = 1
 
@@ -137,9 +140,12 @@ class MyGame(arcade.Window):
             # spawning enemies every x seconds
             if self.enemy_timer >= self.enemy_spawn_time:
                 self.enemy_timer = 0
-                self.new_enemy = Enemy(ENEMY_PATH, 2, 1, .75)
+                if self.score < 45:
+                    self.new_enemy = Enemy(ENEMY1_PATH, "Enemy1", 2, 1)
+                else:
+                    self.new_enemy = Enemy(ENEMY2_PATH, "Enemy2", 4, 2)
                 self.new_enemy.center_x = 880
-                self.new_enemy.center_y = random.randint(27, SCREEN_HEIGHT-27)
+                self.new_enemy.center_y = random.randint(80, SCREEN_HEIGHT - 80)
                 self.enemy_list.append(self.new_enemy)
 
             # enemies fire every 1.5 seconds
@@ -149,31 +155,35 @@ class MyGame(arcade.Window):
                 self.enemy_shooting = self.enemy_list.sprite_list[self.enemy_rand]
                 # only fire if enemy is fully on-screen
                 if self.enemy_shooting.ready:
-                    self.new_shot = Shot(ENEMY_SHOT_PATH, -10, 2)
-                    self.new_shot.center_x = self.enemy_shooting.center_x - 35
+                    self.enemy_shot_sound.play()
+                    if self.enemy_shooting.name == "Enemy1":
+                        self.new_shot = Shot(ENEMY_SHOT_PATH, -7.5, 2)
+                    else:
+                        self.new_shot = Shot(ENEMY_SHOT2_PATH, -10, 2)
+
+                    self.new_shot.right = self.enemy_shooting.left + 10
                     self.new_shot.center_y = self.enemy_shooting.center_y
                     self.enemy_shot_list.append(self.new_shot)
-                    self.enemy_shot_sound.play()
 
     def move_player(self, delta_time):
         # check the flags for movement and update accordingly
-        if self.right:
+        if self.move_right:
             self.player.center_x += self.PLAYER_SPEED * delta_time
-        if self.left:
+        if self.move_left:
             self.player.center_x -= self.PLAYER_SPEED * delta_time
-        if self.up:
+        if self.move_up:
             self.player.center_y += self.PLAYER_SPEED * delta_time
-        if self.down:
+        if self.move_down:
             self.player.center_y -= self.PLAYER_SPEED * delta_time
 
         # prevent movement outside of the window boundaries
-        if self.player.center_x > SCREEN_WIDTH - 38:
+        if self.player.right > SCREEN_WIDTH:
             self.player.center_x -= self.PLAYER_SPEED * delta_time
-        if self.player.center_x < 38:
+        if self.player.left < 0:
             self.player.center_x += self.PLAYER_SPEED * delta_time
-        if self.player.center_y > SCREEN_HEIGHT - 17:
+        if self.player.top > SCREEN_HEIGHT:
             self.player.center_y -= self.PLAYER_SPEED * delta_time
-        if self.player.center_y < 17:
+        if self.player.bottom < 0:
             self.player.center_y += self.PLAYER_SPEED * delta_time
 
     def move_background(self):
@@ -194,16 +204,25 @@ class MyGame(arcade.Window):
                 if self.collisions[0].health == 2:
                     self.enemy_hit_sound.play()
                     self.collisions[0].health -= 1
+                    self.new_enemy = Enemy(ENEMY2_DAMAGED_PATH, self.collisions[0].name, self.collisions[0].speed,
+                                           self.collisions[0].health)
+                    self.new_enemy.center_x = self.collisions[0].center_x
+                    self.new_enemy.center_y = self.collisions[0].center_y
+                    self.enemy_list.append(self.new_enemy)
+                    self.collisions[0].kill()
                     shot.kill()
                 elif self.collisions[0].health == 1:
                     self.enemy_hit_sound.play()
                     self.collisions[0].kill()
                     shot.kill()
-                    self.score += 1
-                    if self.score >= 3:
+                    if self.collisions[0].name == "Enemy1":
+                        self.score += 2
+                    else:
+                        self.score += 3
+
+                    if self.score >= 60:
                         self.win_sound.play()
                         self.state = "WIN"
-                        self.win()
 
         # iterate through enemy shots to check for player collision
         for shot in self.enemy_shot_list:
@@ -213,32 +232,32 @@ class MyGame(arcade.Window):
                     self.player_health -= 1
                     shot.kill()
                 elif self.player_health == 1:
-                    # self.lose_sound.play()
+                    self.player_hit_sound.play()
+                    self.lose_sound.play()
                     self.state = "LOSE"
-                    self.lose()
 
         # check if player is colliding with enemy ships
         if len(self.player.collides_with_list(self.enemy_list)) > 0:
-            # self.lose_sound.play()
+            self.player_hit_sound.play()
+            self.lose_sound.play()
             self.state = "LOSE"
-            self.lose()
 
     def on_key_press(self, symbol, modifiers):
         # check for keyboard input on arrows or W/A/S/D
         if symbol == arcade.key.RIGHT or symbol == arcade.key.D:
-            self.right = True
+            self.move_right = True
         if symbol == arcade.key.LEFT or symbol == arcade.key.A:
-            self.left = True
+            self.move_left = True
         if symbol == arcade.key.UP or symbol == arcade.key.W:
-            self.up = True
+            self.move_up = True
         if symbol == arcade.key.DOWN or symbol == arcade.key.S:
-            self.down = True
+            self.move_down = True
 
         if symbol == arcade.key.SPACE and self.player_shot_timer >= 1:
             self.player_shot_timer = 0
             self.player_shot_sound.play()
             self.new_shot = Shot(PLAYER_SHOT_PATH, 10, 1)
-            self.new_shot.center_x = self.player.center_x + 76
+            self.new_shot.center_x = self.player.right
             self.new_shot.center_y = self.player.center_y
             self.player_shot_list.append(self.new_shot)
 
@@ -248,22 +267,19 @@ class MyGame(arcade.Window):
         if symbol == arcade.key.R and (self.state == "WIN" or self.state == "LOSE"):
             self.setup()
 
+        if symbol == arcade.key.ESCAPE:
+            self.close()
+
     def on_key_release(self, symbol, modifiers):
         # check for keyboard release on arrows or W/A/S/D
         if symbol == arcade.key.RIGHT or symbol == arcade.key.D:
-            self.right = False
+            self.move_right = False
         if symbol == arcade.key.LEFT or symbol == arcade.key.A:
-            self.left = False
+            self.move_left = False
         if symbol == arcade.key.UP or symbol == arcade.key.W:
-            self.up = False
+            self.move_up = False
         if symbol == arcade.key.DOWN or symbol == arcade.key.S:
-            self.down = False
-
-    def lose(self):
-        pass
-
-    def win(self):
-        pass
+            self.move_down = False
 
 
 class Shot(arcade.Sprite):
@@ -278,21 +294,22 @@ class Shot(arcade.Sprite):
 
 
 class Enemy(arcade.Sprite):
-    def __init__(self, filename, speed, health, sprite_scale):
-        super().__init__(filename, scale=sprite_scale)
+    def __init__(self, filename, name, speed, health):
+        super().__init__(filename)
+        self.name = name
         self.speed = speed
         self.health = health
         self.ready = False  # used to track if enemy is done with initial fade onto screen
 
     def update(self):
         # enter screen then move up/down, reversing movement when hitting window boundaries
-        if self.center_x > 770:
+        if self.right > SCREEN_WIDTH:
             self.center_x -= 3
         else:
             if self.ready is False:
                 self.ready = True
             self.center_y += self.speed
-            if self.center_y > SCREEN_HEIGHT - 27 or self.center_y < 27:
+            if self.top > SCREEN_HEIGHT or self.bottom < 0:
                 self.speed *= -1
 
 
